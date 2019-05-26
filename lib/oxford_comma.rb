1c8aacf670b3f712712201374517ae7feef80638
Amanda King <amapickell@gmail.com>
2019-05-26 19:30:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.size == 1
-    puts array.join
+    return array.join
     elsif array.size == 2
-    puts array.join(" and ")
+    return array.join(" and ")
   else
     last = ", and " + array.pop
-    puts array.join(", ") + last
+    return array.join(", ") + last
   end
 end
